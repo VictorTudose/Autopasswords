611eb9a56fad5ac7e63d9595c17b71de4d434aff
Victor Tudose <tudose.victor99@yahoo.com>
2021-05-24 12:20:07 +0300

    file name goes to lcd

--- sketch_may23a/sketch_may23a.ino
+++ sketch_may23a/sketch_may23a.ino
@@ -7,15 +7,24 @@
 #define outputA 6
 #define outputB 7
 
-int index=0;
+int index=1;
 int count=0;
+
 File root;
 File current;
+
 Vector<String> fileContent;
+
+int aState;
+int aLastState;
 
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 void setup() {
+
+
+  Serial.begin(9600);
+  Serial.println("Set up had begun");
 
   String buff="";
 
@@ -25,25 +34,70 @@ void setup() {
   pinMode (outputA,INPUT);
   pinMode (outputB,INPUT);
   
-  Serial.begin(9600);
-
   if (!SD.begin(5)) {
       while (1);
   }
   
 
   root = SD.open("/");
-  
-  //aLastState = digitalRead(outputA);
+
+  Serial.println("Opened root");
+
+  countFiles(root);
+
+  aLastState = digitalRead(outputA);   
+
+  Serial.println("Set up donne");
 
 }
 
+void countFiles(File dir) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    Serial.println(entry.name());
+    count++;
+    entry.close();
+  }
+  Serial.println(count);
+}
+
+
 void loop() {
   
-  lcd.setCursor(2, 0); //
-  lcd.autoscroll(); //enable auto-scrolling
-  //lcd.print(fileContent[0]);
-  //Serial.println(fileContent[0]);
-  delay(5000);
-  lcd.clear();
+   aState = digitalRead(outputA); // Reads the "current" state of the outputA
+   // If the previous and the current state of the outputA are different, that means a Pulse has occured
+   if (aState != aLastState){     
+     lcd.clear();
+     if (digitalRead(outputB) != aState) { 
+       index ++;
+       if(index > count-1)
+        index = 1;
+     } else {
+       index --;
+       if(index < 1)
+        index = count-1;
+     }
+     Serial.print("Position: ");
+     Serial.println(index);
+     openNthFile(index);
+   } 
+   aLastState = aState; // Updates the previous state of the outputA with the current state
+ 
+}
+
+void openNthFile(int n)
+{
+  root.rewindDirectory();
+  for(int i=0;i<n;i++){
+    current = root.openNextFile();
+    current.close();
+  }
+  current = root.openNextFile();
+  lcd.print(current.name());
+  current.close();
 }
